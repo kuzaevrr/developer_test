a4af4b358b0dd07383e7cf8f7df090d15707700e
Ramil <kuzaevrr@mail.ru>
2021-06-22 00:35:10 +0500

    view and handlerClick to sort column.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,90 +1,121 @@
 window.addEventListener("DOMContentLoaded", () => {
+
     const buttonTask = document.querySelector(".btn_task "),
         buttonEmployee = document.querySelector(".btn_emp"),
         tableEmp = document.querySelector(".table_emps"),
         tableTask = document.querySelector(".table_tasks"),
         urlBase = "http://localhost:8080";
 
-    function tableView() {
-        const table_body = document.querySelectorAll('.table_tr');
-
-        table_body.forEach((item, i) => {
-            if (i % 2) {
-                //нечетные
-                item.classList.add("even")
-            } else {
-                item.classList.add("odd")
-            }
-        });
-    }
-
-    createTable(urlBase + "/api/allEmployees", ["ID", "ФИО", "Руководитель", "Филиал", "Количество задач"], "table_emps");
-
-    function createTable(url, arrTitle, tableName) {
-        $(document).ready(function () {
-            $.get(url, function (data) {
-                console.log(data);
-
-                let table =
-                    "<tr class=\"table_tr_root\">\n" +
-                    "<th class=\"table_th\">" + arrTitle[0] + "</th>\n" +
-                    "<th class=\"table_th\">" + arrTitle[1] + "</th>\n" +
-                    "<th class=\"table_th\">" + arrTitle[2] + "</th>\n" +
-                    "<th class=\"table_th\">" + arrTitle[3] + "</th>\n";
-
-                if (tableName == "table_emps") {
-                    table += "<th class=\"table_th\">" + arrTitle[4] + "</th>\n";
-                }
-                table += "</tr>";
-
-                data.forEach(item => {
-
-                    if (tableName == "table_emps") {
-                        if (item.leaderName == null) {
-                            item.leaderName = "";
+    function clickable_sort(buttonTable, buttonSort) {
+        buttonTable.forEach(item =>{
+            item.addEventListener("click", (event) => {
+                const target = event.target;
+                if (target && target.classList.contains("image_sort")) {
+                    buttonSort.forEach((item, i) => {
+                        if (target == item) {
+                            console.log(i);
                         }
-
-                        table +=
-                            "<tr class=\"table_tr\">" +
-                            "<td class=\"table_th\">" + item.id + "</td>" +
-                            "<td class=\"table_th\"> " + item.full_name + "</td>" +
-                            "<td class=\"table_th\">" + item.leaderName + "</td>" +
-                            "<td class=\"table_th\">" + item.branch_name + "</td>" +
-                            "<td class=\"table_th\">" + item.numberTasks + "</td>" +
-                            "</tr>";
-                    } else if (tableName == "table_tasks") {
-                        table +=
-                            "<tr class=\"table_tr\">" +
-                            "<td class=\"table_th\">" + item.id + "</td>" +
-                            "<td class=\"table_th\"> " + item.description + "</td>" +
-                            "<td class=\"table_th\">" + item.nameEmployee + "</td>" +
-                            "<td class=\"table_th\">" + item.priority + "</td>" +
-                            "</tr>";
-                    }
-                });
-                $("." + tableName).html(table);
-                tableView();
+                    });
+                }
             });
         });
     }
 
 
-    buttonTask.addEventListener("click", () => {
-        createTable(urlBase + "/api/allTasks", ["ID", "Название", "Исполнитель", "Приоритет"], "table_tasks");
-        tableEmp.classList.add("none");
-        tableEmp.classList.remove("show");
-        tableTask.classList.remove("none");
-        tableTask.classList.add("show");
+    function tableView() {
+        const table_body = document.querySelectorAll('.table_tr');
 
+        table_body.forEach((item, i) => {
+            if (i % 2) {
+                //четные
+                item.classList.add("even")
+            } else {
+                //нечетные
+                item.classList.add("odd")
+            }
+        });
+    }
+
+
+    function createTable(url, tableName) {
+        $(document).ready(function () {
+            $.get(url, function (data) {
+
+
+                let docRemove = document.querySelectorAll("." + tableName + " .table_tr");
+                if (docRemove != null) {
+                    docRemove.forEach(item => {
+                        item.remove();
+                    });
+                }
+                let table = "";
+                data.forEach(item => {
+
+                    if (tableName === "table_emps") {
+
+                        visible(true);
+
+
+                        if (item.leaderName == null) {
+                            item.leaderName = "";
+                        }
+                        table +=
+                            "<tr class=\"table_tr\">" +
+                            "<td class=\"table_th\">" + item.id + "</td>" +
+                            "<td class=\"table_th\">" + item.full_name + "</td>" +
+                            "<td class=\"table_th\">" + item.leaderName + "</td>" +
+                            "<td class=\"table_th\">" + item.branch_name + "</td>" +
+                            "<td class=\"table_th\">" + item.numberTasks + "</td>" +
+                            "</tr>";
+                    } else if (tableName === "table_tasks") {
+
+                        visible(false);
+                        table +=
+                            "<tr class=\"table_tr\">" +
+                            "<td class=\"table_th\">" + item.id + "</td>" +
+                            "<td class=\"table_th\">" + item.description + "</td>" +
+                            "<td class=\"table_th\">" + item.nameEmployee + "</td>" +
+                            "<td class=\"table_th\">" + item.priority + "</td>" +
+                            "</tr>";
+                    }
+                });
+                document.querySelector("." + tableName).insertAdjacentHTML("beforeEnd", table);
+                tableView();
+            });
+        });
+    }
+
+    function visible(bool) {
+        let arr = ["show", "none"];
+        tableEmp.classList.add(arr[+!bool]);
+        tableTask.classList.remove(arr[+!bool]);
+        tableTask.classList.add(arr[+bool]);
+        tableEmp.classList.remove(arr[+bool]);
+    }
+
+
+
+    createTable(urlBase + "/api/allEmployees", "table_emps");
+    clickable_sort(document.querySelectorAll(".table"),
+        document.querySelectorAll(".image_sort"));
+
+    buttonEmployee.setAttribute("disabled", true);
+
+    buttonTask.addEventListener("click", () => {
+        createTable(urlBase + "/api/allTasks", "table_tasks");
+        buttonTask.setAttribute("disabled", true);
+        buttonEmployee.removeAttribute("disabled");
     });
 
     buttonEmployee.addEventListener("click", () => {
-        createTable(urlBase + "/api/allEmployees", ["ID", "ФИО", "Руководитель", "Филиал", "Количество задач"], "table_emps");
-        tableEmp.classList.add("show");
-        tableEmp.classList.remove("none");
-        tableTask.classList.remove("show");
-        tableTask.classList.add("none");
+        clickable_sort(document.querySelector(".table"),
+            document.querySelectorAll(".image_sort"));
+        createTable(urlBase + "/api/allEmployees", "table_emps");
 
+        buttonEmployee.setAttribute("disabled", true);
+        buttonTask.removeAttribute("disabled");
     });
+
+
 });
 
